1249ae725d17ecccb3fe19f762a102a9ef6ea1bc
Peter Richardson <github@zoomy.net>
2014-08-02 18:18:31 -0400

    gettin fancy with arrays and auto-capitalization

--- Source/content_script.js
+++ Source/content_script.js
@@ -27,17 +27,29 @@ function walk(node)
 	}
 }
 
+var verbs = ["gently tidy up", "fluff the pillow of", "cuddle", "fluster", "mildly agitate", "massage"];
+var gerunds = ["gently tidying up", "fluffing the pillow of", "cuddling", "flustering", "mildly agitating", "massaging"];
+
+function oneOf(array) {
+	var item = array[Math.floor(Math.random()*array.length)];
+}
+
+}
 function handleText(textNode) 
 {
 	var v = textNode.nodeValue;
+	var capped = /[A-Z]/.test( v[0]) );
 
-	v = v.replace(/\bDisrupt\b/g, "Fluster");
-	v = v.replace(/\bdisrupt\b/g, "fluster");
-	v = v.replace(/\bDisruption\b/g, "Flustering");
-	v = v.replace(/\bdisruption\b/g, "flustering");
-	v = v.replace(/\bDisruptive\b/g, "Flustering");
-	v = v.replace(/\bdisruptive\b/g, "flustering");
+	v = v.replace(/\bDisrupt\b/g, oneOf(verbs));
+	v = v.replace(/\bdisrupt\b/g, oneOf(verbs));
+	v = v.replace(/\bDisruption\b/g, oneOf(gerunds));
+	v = v.replace(/\bdisruption\b/g, oneOf(gerunds));
+	v = v.replace(/\bDisruptive\b/g, oneOf(gerunds));
+	v = v.replace(/\bdisruptive\b/g, oneOf(gerunds));
+	v = v.replace(/\bDisrupting\b/g, oneOf(gerunds));
+	v = v.replace(/\bdisrupting\b/g, oneOf(gerunds));
 	
+	if capped { v = v.charAt(0).toUpperCase() + v.slice(1); }
 	textNode.nodeValue = v;
 }
 
